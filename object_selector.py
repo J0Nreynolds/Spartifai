05be982f60570ce8c11328116a73bdd61c5aaf4d
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-27 11:49:18 -0500

    json array returned

--- object_selector.py
+++ object_selector.py
@@ -1,3 +1,6 @@
-results_json = clarifai_api.tag_images([open('/Captures/capture1.jpeg', 'rb'), open('/Captures/capture2.jpeg', 'rb'), open('/Captures/capture3.jpeg', 'rb'), open('/Captures/capture4.jpeg', 'rb'), open('/Captures/capture5.jpeg', 'rb'), open('/Captures/capture6.jpeg', 'rb'), open('/Captures/capture7.jpeg', 'rb'), open('/Captures/capture8.jpeg', 'rb'), open('/Captures/capture9.jpeg', 'rb'), open('/Captures/capture10.jpeg', 'rb')])
-results = results_json.results
+from clarifai.client import ClarifaiApi
+clarifai_api = ClarifaiApi()
+
+results_json = clarifai_api.tag_images([open('Captures/capture1.png', 'rb'), open('Captures/capture2.png', 'rb'), open('Captures/capture3.png', 'rb'), open('Captures/capture4.png', 'rb'), open('Captures/capture5.png', 'rb'), open('Captures/capture6.png', 'rb'), open('Captures/capture7.png', 'rb'), open('Captures/capture8.png', 'rb'), open('Captures/capture9.png', 'rb'), open('Captures/capture10.png', 'rb')])
+results = results_json['results']
 print results

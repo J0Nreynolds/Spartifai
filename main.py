0009135478af56f8463716a3905a63ccc9d06dbf
Jonathan Reynolds <j14reynolds@gmail.com>
2016-02-26 23:38:28 -0500

    Clarifai API test

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 def main():
     pass
+
+from clarifai.client import ClarifaiApi
+clarifai_api = ClarifaiApi() # assumes environment variables are set.
+result = clarifai_api.tag_images(open('sample.jpg', 'rb'))
+print result;

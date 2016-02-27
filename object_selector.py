b8df84ff180f1dde137615925d90e23acc4f4491
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-27 15:01:59 -0500

    holy shit it works

--- object_selector.py
+++ object_selector.py
@@ -22,27 +22,52 @@ def parse_tagged(tagged):
     return non_nouns
 
 def find_objects(results):
-    for x in xrange(0 , len(results)-2):
-    effectiveids = results[x]['result']['tag']['concept_ids'] + results[x+1]['result']['tag']['concept_ids']
-    effectiveprobs = results[x]['result']['tag']['probs'] + results[x+1]['result']['tag']['probs']
+    prominentObjects = []
+    for x in xrange(0 , len(results)-1):
+        highestInfo = []
+        effectiveids = results[x]['result']['tag']['concept_ids'] + results[x+1]['result']['tag']['concept_ids']
+        effectiveprobs = results[x]['result']['tag']['probs'] + results[x+1]['result']['tag']['probs']
+        effectivenames = results[x]['result']['tag']['classes'] + results[x+1]['result']['tag']['classes']
+        adjustedprobs = adjust_probs(effectiveprobs)
 
-    duplicate_pairs = find_duplicates(effectiveids)
-    add_duplicate_probs(duplicate_pairs, effectiveprobs)
+        duplicate_pairs = find_duplicates(effectiveids)
+        adjustedprobs = add_duplicate_probs(duplicate_pairs, adjustedprobs)
+
+        indexOfHighest = find_most_likely(adjustedprobs)
+        highestInfo.append(effectiveids[indexOfHighest])
+        highestInfo.append(effectivenames[indexOfHighest])
+        highestInfo.append(effectiveprobs[indexOfHighest])
+
+        prominentObjects.append(highestInfo)
+    return prominentObjects
+
+def find_most_likely(adjustedprobs):
+    index = 0
+    for x in xrange(0, len(adjustedprobs)):
+        if adjustedprobs[index] < adjustedprobs[x]:
+            index = x
+    return index
+
+def adjust_probs(effectiveprobs):
+    for x in xrange(0,len(effectiveprobs)):
+        effectiveprobs[x] -= .85
+    return effectiveprobs
 
 def find_duplicates(effectiveids):
     D = defaultdict(list)
     for i,item in enumerate(effectiveids):
         D[item].append(i)
     D = {k:v for k,v in D.items() if len(v)>1}
-    pdb.set_trace()
     return D
 
-def add_duplicate_probs(duplicate_pairs, effectiveprobs):
-    for x in xrange(0,len(duplicate_pairs)-1):
+def add_duplicate_probs(duplicate_pairs, adjustedprobs):
+    for x in xrange(0,len(duplicate_pairs)):
         duplicate_pair = duplicate_pairs.itervalues().next()
         index0 = duplicate_pair[0]
         index1 = duplicate_pair[1]
-
+        adjustedprobs[index0] += adjustedprobs[index1]
+        adjustedprobs[index1] = 0
+    return adjustedprobs
 
 clarifai_api = ClarifaiApi()
 
@@ -53,6 +78,7 @@ results = results_json['results']
 for result in results:
     result = return_nouns(result)
 
-print results
+prominentObjects = find_objects(results)
 
-find_objects(results)
+print prominentObjects
+

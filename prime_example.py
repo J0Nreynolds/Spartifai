84e0d75c1f1bfff84617258d1a14290a26401bc8
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-27 06:43:36 -0500

    made output file

--- prime_example.py
+++ prime_example.py
@@ -3,12 +3,17 @@ from primesense import openni2
 openni2.initialize()     # can also accept the path of the OpenNI redistribution
 
 dev = openni2.Device.open_any()
-print dev.get_sensor_info()
+file = open('prime_example_output.txt', 'w')
+# print dev.get_sensor_info()
 
 depth_stream = dev.create_depth_stream()
 depth_stream.start()
 frame = depth_stream.read_frame()
 frame_data = frame.get_buffer_as_uint16()
+dataString = str(frame_data)
+file.write(dataString)
+
+
 depth_stream.stop()
 
 openni2.unload()

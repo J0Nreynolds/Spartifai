5c057b0a22fef2aaac41db60f521b38210436c6f
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-27 12:37:47 -0500

    parsing for nouns

--- object_selector.py
+++ object_selector.py
@@ -1,6 +1,16 @@
 from clarifai.client import ClarifaiApi
+import nltk
+import pdb
+
+def return_nouns(result):
+    words = result['result']['tag']['classes']
+    tagged = nltk.pos_tag(words)
+    parse_tagged(tagged)
+
 clarifai_api = ClarifaiApi()
 
 results_json = clarifai_api.tag_images([open('Captures/capture1.png', 'rb'), open('Captures/capture2.png', 'rb'), open('Captures/capture3.png', 'rb'), open('Captures/capture4.png', 'rb'), open('Captures/capture5.png', 'rb'), open('Captures/capture6.png', 'rb'), open('Captures/capture7.png', 'rb'), open('Captures/capture8.png', 'rb'), open('Captures/capture9.png', 'rb'), open('Captures/capture10.png', 'rb')])
 results = results_json['results']
-print results
+
+for result in results:
+    return_nouns(result)

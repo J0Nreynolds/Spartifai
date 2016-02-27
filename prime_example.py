4871554583fc6d26a24a2c6543e5d858da2fa092
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-27 06:48:49 -0500

    unwrapping array

--- prime_example.py
+++ prime_example.py
@@ -1,15 +1,20 @@
 from primesense import openni2
+import pdb
+import numpy as np
+import matplotlib.pyplot as plt
 
 openni2.initialize()     # can also accept the path of the OpenNI redistribution
 
 dev = openni2.Device.open_any()
 file = open('prime_example_output.txt', 'w')
-# print dev.get_sensor_info()
+print dev.get_sensor_info()
 
 depth_stream = dev.create_depth_stream()
 depth_stream.start()
 frame = depth_stream.read_frame()
 frame_data = frame.get_buffer_as_uint16()
+pdb.set_trace()
+
 dataString = str(frame_data)
 file.write(dataString)
 

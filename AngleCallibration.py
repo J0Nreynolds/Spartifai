d72b1418d6633229f26eab55372a0e515e0c6314
Vignesh Gopal <vignesh.gopal011@gmail.com>
2016-02-27 15:38:25 -0500

    chagnes

--- AngleCallibration.py
+++ AngleCallibration.py
@@ -9,9 +9,21 @@ import time
 ser = serial.Serial('/dev/cu.usbmodem1411', 19200)
 x = int(ser.readline())
 print x
-i = 1
-while x < 180:
-    if int(ser.readline()) - x >= 15:
-        print "%d I hate jews" % i
-        x = int(ser.readline())
-        i = i+1
+
+# i = 1
+# while x < 180:
+#     if int(ser.readline()) - x >= 15:
+#         print "%d I hate jews" % i
+#         x = int(ser.readline())
+#         i = i+1
+#     print int(ser.readline())
+
+done = False
+while not done:
+    x = int(ser.readline())
+    if x%15 ==0:
+        print "%s I hate jews" % str(x/15)
+    if x >= 180:
+        done = True
+
+    print int(ser.readline())

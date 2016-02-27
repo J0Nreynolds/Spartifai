9e8196b50a2d19bb90bcf511257233755dd8264f
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-27 16:13:30 -0500

    at x oclock

--- object_selector.py
+++ object_selector.py
@@ -12,9 +12,11 @@ def main():
     image_array = [open('Captures/capture1.png', 'rb'), open('Captures/capture2.png', 'rb'), open('Captures/capture3.png', 'rb'), open('Captures/capture4.png', 'rb'), open('Captures/capture5.png', 'rb'), open('Captures/capture6.png', 'rb'), open('Captures/capture7.png', 'rb'), open('Captures/capture8.png', 'rb'), open('Captures/capture9.png', 'rb'), open('Captures/capture10.png', 'rb')]
     results_json = clarifai_api.tag_images(image_array)
     results = results_json['results']
+    print "GAY GAY GAY"*1000
 
     for result in results:
         result = return_nouns(result)
+    print "FUCK FUCK FUCK"*1000
 
     all_results, text_results = find_objects(results)
     text_results_string = str(text_results)
@@ -44,6 +46,8 @@ def find_objects(results):
     prominentObjects = []
     namesOfObjects = []
     for x in xrange(0 , len(results)-1):
+        hourRatio = ((2.0*x + 1.0)/2.0)/len(results)
+        timeInHours = put_time_in_hours(hourRatio)
         highestInfo = []
         effectiveids = results[x]['result']['tag']['concept_ids'] + results[x+1]['result']['tag']['concept_ids']
         effectiveprobs = results[x]['result']['tag']['probs'] + results[x+1]['result']['tag']['probs']
@@ -56,11 +60,20 @@ def find_objects(results):
         indexOfHighest = find_most_likely(adjustedprobs)
         highestInfo.append(effectiveids[indexOfHighest])
         highestInfo.append(effectivenames[indexOfHighest])
-        namesOfObjects.append(effectivenames[indexOfHighest])
+        namesOfObjects.append(str(effectivenames[indexOfHighest]) + " at " + str(int(timeInHours)) + " o'clock.")
         highestInfo.append(effectiveprobs[indexOfHighest])
 
         prominentObjects.append(highestInfo)
     return prominentObjects, namesOfObjects
+
+def put_time_in_hours(hourRatio):
+    hours = round(hourRatio*6, 0)
+    adjustedhours = 0
+    if hours>3 :
+        adjustedhours = hours - 3
+    else :
+        adjustedhours = hours + 9
+    return adjustedhours
 
 def find_most_likely(adjustedprobs):
     index = 0

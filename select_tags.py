32cf1352847fd6bd84c59a2809a0a8ffd0d0e2bc
Vignesh Gopal <vignesh.gopal011@gmail.com>
2016-02-28 01:56:24 -0500

    abrar is cute

--- select_tags.py
+++ select_tags.py
@@ -7,9 +7,12 @@ from collections import Counter
 from collections import defaultdict
 import subprocess
 
+def say(string):
+    subprocess.call(['say', string])
+
 def main():
     clarifai_api = ClarifaiApi()
-    image_array = [open('Captures/Capture' + str(x) + ".png") for x in xrange (1,10)]
+    image_array = [open('output/rgb_img_' + str(x) + ".jpg", 'rb') for x in xrange(1,13)]
     results_json = clarifai_api.tag(image_array, select_classes = "animal,car,road,police")
     for x in xrange(len(results_json['results'])):
         results = results_json['results'][x]
@@ -18,7 +21,7 @@ def main():
         classes = tag['classes']
         probs = tag['probs']
         for x in xrange(len(probs)):
-            if probs[x] >= .2:
-                print "Watch out, there is a " + classes[x] + " in front of you"
+            if probs[x] >= .85:
+                say("Watch out, there is a " + str(classes[x]) + " in front of you")
 
 main()

c2fe21469ce6c2a47f66b2efacf45c3c61208d57
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-27 23:02:58 -0500

    this sucks

--- object_selector.py
+++ object_selector.py
@@ -81,19 +81,36 @@ def add_duplicate_probs(duplicate_pairs, adjustedprobs):
         adjustedprobs[index0] += adjustedprobs[index1]
         adjustedprobs[index1] = 0
     return adjustedprobs
-def check_room_qualities(prominentObjects, text_results, names):
-    freqs = Counter(names).items()
-    for i, name in freqs:
-        if freqs[i][1] >= 3:
-            prominentObjects.pop(i)
-            prominentObjects.insert(i, get_information(i))
-    print freqs
+
+def check_room_qualities(prominentObjects, text_results, names, results):
+    D = defaultdict(list)
+    for i,item in enumerate(names):
+        D[item].append(i)
+    D = {k:v for k,v in D.items() if len(v)>1}
+    a = 0
+
+    for x in xrange(0,len(D)):
+        indices = D.itervalues().next()
+        if len(indices) > 2:
+            for index in indices:
+                key = D.keys()[0]
+                unicode_key = unicode(key, "utf-8")
+                this_index = results[index]['result']['tag']['classes'].index(unicode_key)
+                results[this_index]['result']['tag']['concept_ids'].pop(0)
+                results[this_index]['result']['tag']['classes'].pop(0)
+                results[this_index]['result']['tag']['probs'].pop(0)
+
+                prominentObjects.pop(index-a)
+                text_results.pop(index-a)
+                prominentObjects.insert(index-a, get_information(this_index))
+                a += 1
+    text_results, names = find_objects(results)
 
 prominentObjects = []
 namesOfObjects = []
 names = []
 results = main()
 text_results, names = find_objects(results)
-check_room_qualities(prominentObjects, text_results, names)
+check_room_qualities(prominentObjects, text_results, names, results)
 text_results_string = str(text_results)
 espeak(text_results_string)
